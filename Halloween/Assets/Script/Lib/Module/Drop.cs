272c36da63bc67e0e9b01d999d92511a107d73b7
SIrona <mikan.zuki319@gmail.com>
2025-12-06 15:35:54 +0900

    ランダムドロップ作成 ドロップ作成

--- Halloween/Assets/Script/Lib/Module/Drop.cs
+++ Halloween/Assets/Script/Lib/Module/Drop.cs
@@ -3,26 +3,17 @@
 public class Drop : MonoBehaviour
 {
     [SerializeField] GameObject go;
-    //[SerializeField] AudioResource audioSe;
 
     bool isDropEnd = false;
-
-    //確率変数など
-
-    //ランダムにする？
-    private void Start()
-    {
-     
-    }
 
     private void Update()
     {
         if(!GetComponent<EnemyBase>().isDead) return;
         if(isDropEnd) return;
+        isDropEnd = true;
 
         Instantiate(go, transform.position, Quaternion.identity);
-        isDropEnd = true;
-        
+
     }
 
 }
--- Halloween/Assets/Script/MoveCtr/DOJumpMove.cs
+++ Halloween/Assets/Script/MoveCtr/DOJumpMove.cs
@@ -1,5 +1,4 @@
 ﻿using DG.Tweening;
-using System;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
